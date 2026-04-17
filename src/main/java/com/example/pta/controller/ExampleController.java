9221f9f48b2b633253632bfdae19a6d98fb70123
psh153 <tmdgnsdl15345@naver.com>
2026-04-17 17:30:10 +0900

    tiles수정, 로그인 페이지 추가

--- src/main/java/com/example/pta/controller/ExampleController.java
+++ src/main/java/com/example/pta/controller/ExampleController.java
@@ -7,26 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.pta.service.TestService;
 
+@RequestMapping("/service")
 @Controller
 public class ExampleController {
 
     @Autowired
     private TestService testService;
 
-    @RequestMapping("/main")
-    public String ExampleMain(Model model){
-        model.addAttribute("test", testService.dbCheck());
-        return "main";
-    }
-    
-    @RequestMapping("/main2")
-    public String ExampleMain2(Model model){
-    	model.addAttribute("test", testService.dbCheck());
-    	return "main2";
-    }
     @RequestMapping("/list")
     public String tabulator(Model model){
-    	
-    	return "tabulator";
+    		return "program";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<link rel="stylesheet" href="/resources/css/login.css">
+
+<div class="container">
+  <%-- Sign In --%>
+  <div class="container__form container--signin">
+    <form action="#" class="form" id="form2">
+      <h2 class="form__title">로그인</h2>
+      <input type="text" placeholder="Id" class="input" />
+      <input type="password" placeholder="Password" class="input" />
+      <a href="#" class="link">비밀번호찾기</a>
+      <button class="btn">로그인</button>
+    </form>
+  </div>
+</div>
